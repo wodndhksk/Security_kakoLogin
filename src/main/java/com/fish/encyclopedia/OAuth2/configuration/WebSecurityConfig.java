edfaf95964243d71948e38f7ac3e5b0fc7014a75
Jaewoo Kim <wodndhksk@gmail.com>
2022-02-05 18:24:25 +0900

    yml commit

---
+++ src/main/java/com/fish/encyclopedia/OAuth2/configuration/WebSecurityConfig.java
@@ -0,0 +1,21 @@
+package com.fish.encyclopedia.OAuth2.configuration;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+@Configuration
+@Slf4j
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.httpBasic().disable()
+                .csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeRequests()
+                .anyRequest().authenticated();
+    }
+}
