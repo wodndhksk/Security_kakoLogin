894b0168fe6d83d8c9cc34186d7c697b53cd7250
Seong-Jaeho <jaeho.seong@lighthouse.love>
2022-02-27 15:51:50 +0900

    update(entity): modify user entity from ERD

---
+++ src/main/java/com/fish/encyclopedia/database/entity/User.java
@@ -0,0 +1,50 @@
+package com.fish.encyclopedia.database.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@Builder
+@AllArgsConstructor
+@Table(name = "tbl_user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column
+    private Long id;
+
+    @Column
+    @NotBlank
+    private String username;
+
+    @Column
+    @NotBlank
+    private String phone;
+
+    @Column
+    @NotBlank
+    private String name;
+
+    @Column
+    @NotBlank
+    private String email;
+
+    @Column
+    @NotBlank
+    @Enumerated(EnumType.STRING)
+    private SocialType socialType;
+
+    @Column(length = 2050)
+    private String thumbnailImageUrl;
+
+    @Column(length = 2050)
+    private String ProfileImageUrl;
+}
--- src/main/java/com/fish/encyclopedia/database/repository/UserRepository.java
+++ src/main/java/com/fish/encyclopedia/database/repository/UserRepository.java
@@ -1,12 +1,12 @@
 package com.fish.encyclopedia.database.repository;
 
-import com.fish.encyclopedia.database.entity.Account;
+import com.fish.encyclopedia.database.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-public interface AccountRepository extends JpaRepository<Account, Long> {
-    Optional<Account> findByUsername(String username);
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
 }
