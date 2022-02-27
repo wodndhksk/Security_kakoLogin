7449203bed69dd784c553c4433f644b462069369
Seong-Jaeho <jaeho.seong@lighthouse.love>
2022-02-27 16:27:56 +0900

    update(entity): 낚시도감 entity 추가

---
+++ src/main/java/com/fish/encyclopedia/database/entity/UserFishingCollection.java
@@ -0,0 +1,48 @@
+package com.fish.encyclopedia.database.entity;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@Builder
+@AllArgsConstructor
+@Table(name = "tbl_fishing_collection")
+public class UserFishingCollection {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", referencedColumnName = "id")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "fishing_master_id", referencedColumnName = "id")
+    private FishingMaster fishingMaster;
+
+    @Column
+    private LocalDateTime beginAt;
+
+    @Column
+    private LocalDateTime endAt;
+
+    @Column
+    private String content;
+
+    @Column
+    private String fishSpecies;
+
+    @Column
+    private Integer fishLength;
+
+    @Column
+    private Integer fishWeight;
+}
